899bfa91870d9ad5ba2fb3b209a6a368cdb57a8b
dsomorov <dsomorov@gmail.com>
2023-05-11 15:11:17 -0500

    create and connect session and users Express routers

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,32 +1,15 @@
 const router = require('express').Router();
 
-router.post('/test', function (req, res) {
+const { restoreUser } = require("../../utils/auth.js");
+router.use(restoreUser);
+
+const sessionRouter = require("./session.js");
+const usersRouter = require("./users.js");
+router.use("/session", sessionRouter);
+router.use("/users", usersRouter);
+
+router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
 });
-
-
-const { setTokenCookie, restoreUser, requireAuth } = require("../../utils/auth.js");
-
-//testing setTokenCookie
-const { User } = require("../../db/models");
-router.get("/set-token-cookie", async (req, res) => {
-  const user = await User.findOne({
-    where: { username: "User1" }
-  });
-  setTokenCookie(res, user);
-  return res.json({ user });
-});
-
-// testing restoreUser
-router.use(restoreUser);
-router.get("/restore-user", (req, res) => {
-  return res.json(req.user);
-});
-
-// testing requireAuth
-router.get("/require-auth", requireAuth, (req, res) => {
-  return res.json(req.user);
-});
-
 
 module.exports = router;
