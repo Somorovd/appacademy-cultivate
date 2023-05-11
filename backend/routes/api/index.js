456ed9a1c0831e52c237e56e6ba80a580a3e7be1
dsomorov <dsomorov@gmail.com>
2023-05-11 14:57:49 -0500

    Add and test user authentication express middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,4 +4,29 @@ router.post('/test', function (req, res) {
   res.json({ requestBody: req.body });
 });
 
+
+const { setTokenCookie, restoreUser, requireAuth } = require("../../utils/auth.js");
+
+//testing setTokenCookie
+const { User } = require("../../db/models");
+router.get("/set-token-cookie", async (req, res) => {
+  const user = await User.findOne({
+    where: { username: "User1" }
+  });
+  setTokenCookie(res, user);
+  return res.json({ user });
+});
+
+// testing restoreUser
+router.use(restoreUser);
+router.get("/restore-user", (req, res) => {
+  return res.json(req.user);
+});
+
+// testing requireAuth
+router.get("/require-auth", requireAuth, (req, res) => {
+  return res.json(req.user);
+});
+
+
 module.exports = router;
