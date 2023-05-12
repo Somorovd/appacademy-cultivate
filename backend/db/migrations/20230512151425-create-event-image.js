4f1b572978e36630eb2a1679ab1099ccec2e5151
dsomorov <dsomorov@gmail.com>
2023-05-12 10:22:11 -0500

    create EventImage migration and model

---
+++ backend/db/migrations/20230512151425-create-event-image.js
@@ -0,0 +1,38 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('EventImages', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      eventId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        // references: { model: "Events" }
+      },
+      url: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      preview: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('EventImages');
+  }
+};
