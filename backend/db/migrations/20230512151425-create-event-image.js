0e53a9e6c278404a6477b3dfdc71b8289ad05da2
dsomorov <dsomorov@gmail.com>
2023-05-12 10:38:40 -0500

    add options {} for production on EventImages and Venues migrations

--- backend/db/migrations/20230512151425-create-event-image.js
+++ backend/db/migrations/20230512151425-create-event-image.js
@@ -1,5 +1,11 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('EventImages', {
@@ -35,6 +41,7 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('EventImages');
+    options.tableName = "EventImages";
+    await queryInterface.dropTable(options);
   }
 };
