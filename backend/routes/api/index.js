7acd0e6bd55dc40b0f075636499996f481189b66
dsomorov <dsomorov@gmail.com>
2023-05-12 17:58:15 -0500

    add and connect groups router

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,8 +5,10 @@ router.use(restoreUser);
 
 const sessionRouter = require("./session.js");
 const usersRouter = require("./users.js");
+const groupsRouter = require("./groups.js");
 router.use("/session", sessionRouter);
 router.use("/users", usersRouter);
+router.use("/groups", groupsRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
