83cf1a2c89625e8ede4d47a0451b6822b58bd1c8
dsomorov <dsomorov@gmail.com>
2023-05-17 10:54:33 -0500

    add removeIndex to the down functionof the events table

--- backend/db/migrations/20230511175360-create-event.js
+++ backend/db/migrations/20230511175360-create-event.js
@@ -3,71 +3,72 @@
 
 let options = {};
 if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;
+	options.schema = process.env.SCHEMA;
 }
 
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Events', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      venueId: {
-        type: Sequelize.INTEGER,
-        references: { model: "Venues" }
-      },
-      groupId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: { model: "Groups" },
-        onDelete: "cascade"
-      },
-      name: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      description: {
-        type: Sequelize.STRING
-      },
-      type: {
-        type: Sequelize.ENUM("in person", "online"),
-        defaultValue: "in person"
-      },
-      capacity: {
-        type: Sequelize.INTEGER,
-        allowNull: false
-      },
-      price: {
-        type: Sequelize.FLOAT,
-        defaultValue: 0
-      },
-      startDate: {
-        type: Sequelize.DATE,
-        allowNull: false
-      },
-      endDate: {
-        type: Sequelize.DATE,
-        allowNull: false
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
-      }
-    }, options);
-    options.tableName = "Events";
-    await queryInterface.addIndex(options, ["groupId", "name"], { unique: true });
-  },
-  async down(queryInterface, Sequelize) {
-    options.tableName = "Events";
-    await queryInterface.dropTable(options);
-  }
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable('Events', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+			},
+			venueId: {
+				type: Sequelize.INTEGER,
+				references: { model: "Venues" }
+			},
+			groupId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				references: { model: "Groups" },
+				onDelete: "cascade"
+			},
+			name: {
+				type: Sequelize.STRING,
+				allowNull: false
+			},
+			description: {
+				type: Sequelize.STRING
+			},
+			type: {
+				type: Sequelize.ENUM("in person", "online"),
+				defaultValue: "in person"
+			},
+			capacity: {
+				type: Sequelize.INTEGER,
+				allowNull: false
+			},
+			price: {
+				type: Sequelize.FLOAT,
+				defaultValue: 0
+			},
+			startDate: {
+				type: Sequelize.DATE,
+				allowNull: false
+			},
+			endDate: {
+				type: Sequelize.DATE,
+				allowNull: false
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+			}
+		}, options);
+		options.tableName = "Events";
+		await queryInterface.addIndex(options, ["groupId", "name"], { unique: true });
+	},
+	async down(queryInterface, Sequelize) {
+		options.tableName = "Events";
+		await queryInterface.dropTable(options);
+		await queryInterface.removeIndex(options, ["groupId", "name"]);
+	}
 };
