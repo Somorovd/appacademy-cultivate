e1a2133bc282328cbbf2944a2e7317ba3c1d975b
dsomorov <dsomorov@gmail.com>
2023-06-02 15:47:05 -0500

    feat: add basic styling to LoginPage

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import * as sessionActions from "../../store/session";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
@@ -9,44 +9,51 @@ function LoginFormPage() {
   const sessionUser = useSelector((state) => state.session.user);
   const [credential, setCredential] = useState("");
   const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState({});
+  const [validationErrors, setValidationErrors] = useState({});
+  const [isEnabled, setIsEnabled] = useState(false);
+
+  useEffect(() => {
+    setIsEnabled(
+      (credential && credential.length >= 4) &&
+      (password && password.length >= 6)
+    );
+  }, [credential, password])
 
   if (sessionUser) return <Redirect to="/" />;
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setErrors({});
+    setValidationErrors({});
     return dispatch(sessionActions.thunkCreateSession({ credential, password }))
       .catch(async (res) => {
         const data = await res.json();
-        if (data && data.errors) setErrors(data.errors);
+        if (data && data.errors) setValidationErrors(data.errors);
       });
   };
 
   return (
     <>
       <h1>Log In</h1>
+      {validationErrors.credential && <p className="error">{validationErrors.credential}</p>}
       <form onSubmit={handleSubmit}>
-        <label>
-          Username or Email
-          <input
-            type="text"
-            value={credential}
-            onChange={(e) => setCredential(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        {errors.credential && <p>{errors.credential}</p>}
-        <button type="submit">Log In</button>
+        <input
+          type="text"
+          placeholder="Username or Email"
+          value={credential}
+          onChange={(e) => setCredential(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button
+          type="submit"
+          disabled={!isEnabled}
+        >Log In</button>
       </form>
     </>
   );
