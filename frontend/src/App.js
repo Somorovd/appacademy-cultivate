563ff23c313ca7193f6b1df0a6470bda4e9f41c2
dsomorov <dsomorov@gmail.com>
2023-06-02 14:45:09 -0500

    update login page to account for session state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
+import { useDispatch } from "react-redux";
+import * as sessionActions from "./store/session";
 
 function App() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    dispatch(sessionActions.thunkGetSession())
+      .then(() => setIsLoaded(true));
+  }, [dispatch]);
+
   return (
-    <Switch>
-      <Route path="/login">
-        <LoginFormPage />
-      </Route>
-    </Switch>
+    isLoaded && (
+      <Switch>
+        <Route path="/login">
+          <LoginFormPage />
+        </Route>
+      </Switch>
+    )
   );
 }
 
