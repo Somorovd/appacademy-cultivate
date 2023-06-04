3c7d88fb305b98620af371f410ffdcac22907bb0
dsomorov <dsomorov@gmail.com>
2023-06-04 18:29:07 -0500

    update sign up form validation

--- frontend/src/components/SignupFormModal/index.js
+++ frontend/src/components/SignupFormModal/index.js
@@ -14,7 +14,7 @@ function SignupFormModal() {
   const [lastName, setLastName] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [errors, setErrors] = useState({});
+  const [validationErrors, setValidationErrors] = useState({});
   const [isEnabled, setIsEnabled] = useState(false);
   const { closeModal } = useModal();
 
@@ -26,12 +26,39 @@ function SignupFormModal() {
     );
   }, [email, username, firstName, lastName, password, confirmPassword])
 
+  const validateUsername = () => {
+    const errors = { ...validationErrors };
+    if (!username || username.length >= 4)
+      errors["username"] = "";
+    else
+      errors["username"] = "Username must contain at least 4 characters";
+    setValidationErrors(errors);
+  }
+
+  const validatePassword = () => {
+    const errors = { ...validationErrors };
+    if (!password || password.length >= 6)
+      errors["password"] = "";
+    else
+      errors["password"] = "Password must contain at least 6 characters";
+    setValidationErrors(errors);
+  }
+
+  const validateConfirmPassword = () => {
+    const errors = { ...validationErrors };
+    if (confirmPassword === password)
+      errors["confirmPassword"] = "";
+    else
+      errors["confirmPassword"] = "Confirm Password field must be the same as the Password field"
+    setValidationErrors(errors);
+  }
+
   if (sessionUser) return <Redirect to="/" />;
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (password === confirmPassword) {
-      setErrors({});
+      setValidationErrors({});
       return dispatch(sessionActions.thunkCreateUser({
         email,
         username,
@@ -43,11 +70,11 @@ function SignupFormModal() {
         .catch(async (res) => {
           const data = await res.json();
           if (data && data.errors) {
-            setErrors(data.errors);
+            setValidationErrors(data.errors);
           }
         });
     }
-    return setErrors({
+    return setValidationErrors({
       confirmPassword: "Confirm Password field must be the same as the Password field"
     });
   };
@@ -63,15 +90,16 @@ function SignupFormModal() {
           onChange={(e) => setEmail(e.target.value)}
           required
         />
-        {errors.email && <p className="error">{errors.email}</p>}
+        {validationErrors.email && <p className="error">{validationErrors.email}</p>}
         <input
           type="text"
           placeholder="Username"
           value={username}
-          onChange={(e) => setUsername(e.target.value)}
+          onChange={(e) => setUsername(e.target.value.trim())}
+          onBlur={(e) => validateUsername()}
           required
         />
-        {errors.username && <p className="error">{errors.username}</p>}
+        {validationErrors.username && <p className="error">{validationErrors.username}</p>}
         <input
           type="text"
           placeholder="First Name"
@@ -79,7 +107,7 @@ function SignupFormModal() {
           onChange={(e) => setFirstName(e.target.value)}
           required
         />
-        {errors.firstName && <p className="error">{errors.firstName}</p>}
+        {validationErrors.firstName && <p className="error">{validationErrors.firstName}</p>}
         <input
           type="text"
           placeholder="Last Name"
@@ -87,23 +115,25 @@ function SignupFormModal() {
           onChange={(e) => setLastName(e.target.value)}
           required
         />
-        {errors.lastName && <p className="error">{errors.lastName}</p>}
+        {validationErrors.lastName && <p className="error">{validationErrors.lastName}</p>}
         <input
           type="password"
           placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          onBlur={(e) => validatePassword()}
           required
         />
-        {errors.password && <p className="error">{errors.password}</p>}
+        {validationErrors.password && <p className="error">{validationErrors.password}</p>}
         <input
           type="password"
           placeholder="Confirm Password"
           value={confirmPassword}
           onChange={(e) => setConfirmPassword(e.target.value)}
+          onBlur={(e) => validateConfirmPassword()}
           required
         />
-        {errors.confirmPassword && <p className="error">{errors.confirmPassword}</p>}
+        {validationErrors.confirmPassword && <p className="error">{validationErrors.confirmPassword}</p>}
         <button
           type="submit"
           disabled={!isEnabled}
