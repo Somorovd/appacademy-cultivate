549aa850e1d0bbd075841a882206b0c38ac31315
dsomorov <dsomorov@gmail.com>
2023-06-07 11:31:16 -0500

    update models/migrations to TEXT

--- backend/db/migrations/20230511175360-create-event.js
+++ backend/db/migrations/20230511175360-create-event.js
@@ -30,7 +30,7 @@ module.exports = {
 				allowNull: false
 			},
 			description: {
-				type: Sequelize.STRING,
+				type: Sequelize.TEXT,
 				allowNull: false
 			},
 			type: {
--- backend/db/migrations/20230512151425-create-event-image.js
+++ backend/db/migrations/20230512151425-create-event-image.js
@@ -22,7 +22,7 @@ module.exports = {
 				onDelete: "cascade"
 			},
 			url: {
-				type: Sequelize.STRING,
+				type: Sequelize.TEXT,
 				allowNull: false
 			},
 			preview: {
