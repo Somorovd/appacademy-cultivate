398b7ba64ff02ca8a5178a7f2d366775bc73a99b
dsomorov <dsomorov@gmail.com>
2023-06-08 16:32:08 -0500

    styling landing page

--- frontend/src/components/Pages/LandingPage/index.js
+++ frontend/src/components/Pages/LandingPage/index.js
@@ -13,83 +13,79 @@ const LandingPage = () => {
   return (
     <>
       <div className="page-wrapper">
-        <article className="landing-intro">
-          <div className="landing-intro__text">
-            <h1 className="landing-intro__title">
-              The cultist platform <span className="no-space">---</span> Where prayers become nightmares
-            </h1>
+        <div className="landing-page">
+          <article className="landing-intro">
+            <div className="landing-intro__text">
+              <h1 className="landing-intro__title">
+                The cultist platform 	&mdash; Where prayers become nightmares
+              </h1>
+              <p className="landing-intro__p">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua.
+              </p>
+            </div>
+            <img src="" className="landing-intro__image" />
+          </article>
+          <article className="landing-about">
+            <h2>How to Cultivate</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-              sed do eiusmod tempor incididunt ut labore et dolore magna
-              aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-              ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-              aute irure dolor in reprehenderit in voluptate velit esse
-              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-              cupidatat non proident, sunt in culpa qui officia deserunt
-              mollit anim id est laborum.
+              Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+              consectetur, adipisci velit
             </p>
-          </div>
-          <img src="" className="landing-intro__image" />
-        </article>
-        <article className="landing-about">
-          <h2>How to Cultivate</h2>
-          <p>
-            Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
-            consectetur, adipisci velit, sed quia non numquam eius modi tempora
-            incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
-          </p>
-        </article>
-        <article className="landing-nav">
-          <ul className="landing-nav-list">
-            < li className="landing-nav-card">
-              <img src="" alt="" className="landing-nav-card__image" />
-              <h2
-                className="landing-nav-card__title"
-                onClick={() => { history.push("/groups") }}
-              >
-                See all cults
-              </h2>
-              <p className="landing-nav-card__about">
-                Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
-                consectetur, adipisci velit
-              </p>
-            </li>
-            <li className="landing-nav-card">
-              <img src="" alt="" className="landing-nav-card__image" />
-              <h2
-                className="landing-nav-card__title"
-                onClick={() => { history.push("/events") }}
-              >
-                Find a ritual
-              </h2>
-              <p className="landing-nav-card__about">
-                Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
-                consectetur, adipisci velit
-              </p>
-            </li>
-            <li className="landing-nav-card">
-              <img src="" alt="" className="landing-nav-card__image" />
-              <h2
-                className={startGroupClassName}
-                onClick={() => { history.push("/groups/new") }}
-              >
-                Start a new cult
-              </h2>
-              <p className="landing-nav-card__about">
-                Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
-                consectetur, adipisci velit
-              </p>
-            </li>
-          </ul>
-        </article>
-        {!session.user &&
-          <div className="landing-join">
-            <button>
-              Join Us
-            </button>
-          </div>
-        }
-      </div >
+          </article>
+          <article className="landing-nav">
+            <ul className="landing-nav-list">
+              < li className="landing-nav-card">
+                <img src="" alt="" className="landing-nav-card__image" />
+                <h2
+                  className="landing-nav-card__title"
+                  onClick={() => { history.push("/groups") }}
+                >
+                  See all cults
+                </h2>
+                <p className="landing-nav-card__about">
+                  Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+                  consectetur, adipisci velit
+                </p>
+              </li>
+              <li className="landing-nav-card">
+                <img src="" alt="" className="landing-nav-card__image" />
+                <h2
+                  className="landing-nav-card__title"
+                  onClick={() => { history.push("/events") }}
+                >
+                  Find a ritual
+                </h2>
+                <p className="landing-nav-card__about">
+                  Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+                  consectetur, adipisci velit
+                </p>
+              </li>
+              <li className="landing-nav-card">
+                <img src="" alt="" className="landing-nav-card__image" />
+                <h2
+                  className={startGroupClassName}
+                  onClick={() => { history.push("/groups/new") }}
+                >
+                  Start a new cult
+                </h2>
+                <p className="landing-nav-card__about">
+                  Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+                  consectetur, adipisci velit
+                </p>
+              </li>
+            </ul>
+          </article>
+          {!session.user &&
+            <div className="landing-join">
+              <button>
+                Join Us
+              </button>
+            </div>
+          }
+        </div >
+      </div>
     </>
   )
 }
