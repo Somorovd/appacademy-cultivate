1c22102f89c9f910633f06b9ba3127e31e3e0e55
dsomorov <dsomorov@gmail.com>
2023-07-29 23:37:46 -0500

    add backend route to update preview image

--- backend/routes/api/groupImages.js
+++ backend/routes/api/groupImages.js
@@ -3,9 +3,45 @@ const {
   requireAuth,
   buildAuthorzationErrorResponce,
 } = require("../../utils/auth");
+const { Op } = require("sequelize");
 const { GroupImage, Group, User } = require("../../db/models");
 const { buildMissingResourceError } = require("../../utils/helpers");
 const router = express.Router();
+
+//#region               PUT requests
+router.put("/:imageId", requireAuth, async (req, res, next) => {
+  console.log("here is put route");
+  const imageId = req.params.imageId;
+  const userId = req.user.id;
+
+  const groupImages = await GroupImage.findAll({
+    include: {
+      model: Group.scope({ method: ["includeAuthorization", userId] }),
+    },
+    where: {
+      [Op.or]: {
+        preview: true,
+        id: imageId,
+      },
+    },
+  });
+
+  const groupImage = groupImages.find((img) => img.id === Number(imageId));
+  if (!groupImage) return buildMissingResourceError(next, "Image");
+
+  const group = groupImage["Group"];
+  const isNotAuthorized = group.organizerId != userId && !group["Members"][0];
+  if (isNotAuthorized) return buildAuthorzationErrorResponce(next);
+
+  for (let groupImage of groupImages) {
+    groupImage.set({ preview: !groupImage.preview });
+    await groupImage.save();
+  }
+
+  return res.json({ groupImage });
+});
+
+//#endregion
 
 //#region               DELETE requests
 router.delete("/:imageId", requireAuth, async (req, res, next) => {
@@ -19,6 +55,12 @@ router.delete("/:imageId", requireAuth, async (req, res, next) => {
   });
 
   if (!groupImage) return buildMissingResourceError(next, "Image");
+  if (groupImage.preview) {
+    const err = new Error("Cannot delete preview image");
+    err.title = "Forbidden";
+    err.status = 403;
+    return next(err);
+  }
 
   const group = groupImage["Group"];
   const isNotAuthorized = group.organizerId != userId && !group["Members"][0];
@@ -27,5 +69,7 @@ router.delete("/:imageId", requireAuth, async (req, res, next) => {
   await groupImage.destroy();
   return res.json({ message: "Successfully deleted" });
 });
+
+//#endregion
 
 module.exports = router;
